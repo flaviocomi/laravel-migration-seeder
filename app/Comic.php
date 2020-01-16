d87d0ce22c2cae3ec71fa9c180b1665f56b6265e
flaviocomi <flabio.co91@gmail.com>
2020-01-16 21:17:44 +0100

    project complete

---
+++ app/Comic.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comic extends Model
+{
+    protected $table = 'comics';
+
+    protected $fillable = [
+        'title',
+        'author',
+        'year',
+        'review',
+        'vote'
+    ];
+}
---
+++ app/Http/Controllers/ComicsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comic;
+use Illuminate\Http\Request;
+
+class ComicsController extends Controller
+{
+    public function index()
+    {
+        $comics = Comic::all();
+
+        return view('comic', compact('comics'));
+    }
+}
